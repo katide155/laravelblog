33ee690232ad32b844b78f64a5af2ce915075da9
katide155 <93394912+katide155@users.noreply.github.com>
2022-11-23 15:33:50 +0200

    blog index

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -10,29 +10,29 @@
     </div>
 </div>
 
-
+@foreach ($posts as $post)
     <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200">
         <div>
             <img src="" alt="">
         </div>
         <div>
             <h2 class="text-gray-700 font-bold text-5xl pb-4">
-                
+                 {{ $post->title }}
             </h2>
             <span class="text-gray-500">
-                By <span class="font-bold italic text-gray-800"></span>, Created on 
+                By <span class="font-bold italic text-gray-800">{{ $post->user->name }}</span>, Created on {{ date('jS M Y', strtotime($post->updated_at)) }}
             </span>
 
             <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">
-               
+               {{ $post->description }}
             </p>
 
-            <a href="" class="uppercase bg-blue-500 text-gray-100 text-lg font-extrabold py-4 px-8 rounded-3xl">
+            <a href="/blog/{{ $post->slug }}" class="uppercase bg-blue-500 text-gray-100 text-lg font-extrabold py-4 px-8 rounded-3xl">
                 Keep Reading
             </a>
 
 
         </div>
     </div>  
-
+@endforeach
 @endsection
